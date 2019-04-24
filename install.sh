f90948a042472d8f2076f65de11ad26747604b8a
Czerwinski <kczerwinski@DICE-KCZERWINS2.ad.ea.com>
2019-04-24 14:22:31 +0200

    Install op and check if the dirs exist

--- install.sh
+++ install.sh
@@ -1,28 +1,47 @@
 #!/usr/bin/env bash
 set -e
 
+TEMP_DIR=$(mktemp -d)
+trap "rm -rf ${TEMP_DIR}" EXIT
+
 # Install zsh and tmux
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo apt update
     sudo apt install -y zsh tmux
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  brew install zsh tmux
+  brew install zsh tmux gpg
 fi
+
+# Install 1password cli
+OP_VERSION=${OP_VERSION:-"v0.5.5"}
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    OP_NAME="op_linux_amd64_${OP_VERSION}.zip"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    OP_NAME="op_darwin_amd64_${OP_VERSION}.zip"
+fi
+pushd $TEMP_DIR
+  curl -o $OP_NAME https://cache.agilebits.com/dist/1P/op/pkg/${OP_VERSION}/${OP_NAME}
+  unzip $OP_NAME -d $TEMP_DIR
+  gpg --receive-keys 3FEF9748469ADBE15DA7CA80AC2D62742012EA22
+  gpg --verify op.sig op
+
+  mv op /usr/local/bin/op
+popd
 
 # Install tmux-resurect
 mkdir -p $HOME/.tmux/plugins
-git clone https://github.com/tmux-plugins/tmux-resurrect $HOME/.tmux/plugins/tmux-resurrect
-git clone https://github.com/tmux-plugins/tmux-continuum $HOME/.tmux/plugins/tmux-continuum
+if [ ! -d $HOME/.tmux/plugins/tmux-resurrect ]; then git clone https://github.com/tmux-plugins/tmux-resurrect $HOME/.tmux/plugins/tmux-resurrect ; fi
+if [ ! -d $HOME/.tmux/plugins/tmux-continuum ]; then git clone https://github.com/tmux-plugins/tmux-continuum $HOME/.tmux/plugins/tmux-continuum ; fi
 
-# Install oh my zsh
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-sudo sed -i s#${HOME}:/bin/bash#${HOME}:/bin/zsh#g /etc/passwd
+# # Install oh my zsh
+if [ ! -d $HOME/.oh-my-zsh ]; then git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh ; fi
+# sudo sed -i s#${HOME}:/bin/bash#${HOME}:/bin/zsh#g /etc/passwd
 
 # Checkout configuration
 REPO="https://github.com/kamilczerw/.cfg"
 
-git clone --bare $REPO $HOME/.cfg
+if [ ! -d $HOME/.cfg ]; then git clone --bare $REPO $HOME/.cfg ; fi
 config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 
 $config checkout
