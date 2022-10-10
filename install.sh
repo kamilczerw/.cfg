e4684505023d098512fa16ca2a6a53ea5613ce75
Kamil Czerwiński <kamil@czerwinski.dev>
2022-10-10 09:01:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,8 +10,9 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo apt install -y tmux kubectl
     sudo snap install go
 elif [[ "$OSTYPE" == "darwin"* ]]; then
+  export PATH="$PATH:/opt/homebrew/bin"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  brew install zsh tmux gpg jenv go kubectl
+  brew install zsh gpg jenv go kubectl yq jq fzf jump
 fi
 
 
@@ -29,7 +30,7 @@ if [ ! -d $HOME/.cfg ]; then
   $config checkout
   $config config --local status.showUntrackedFiles no
   $config config user.name "Kamil Czerwiński"
-  $config config user.email "kam.czerwinski@gmail.com"
+  $config config user.email "kamil@czerwinski.dev"
 
   git config --global include.path "${HOME}/.git-conf/config"
 else
@@ -42,9 +43,9 @@ if [ ! -d $HOME/.krew ]; then
     set -x; cd "$(mktemp -d)" &&
     OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
     ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
-    curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.tar.gz" &&
-    tar zxvf krew.tar.gz &&
     KREW=./krew-"${OS}_${ARCH}" &&
+    curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+    tar zxvf "${KREW}.tar.gz" &&
     "$KREW" install krew
     "$KREW" install ctx
     "$KREW" install ns
@@ -55,28 +56,6 @@ fi
 # if [[ "$OSTYPE" == "darwin"* ]]; then
   # brew cask install sequel-pro visual-studio-code clipy
 # fi
-
-# Install jump
-which jump > /dev/null
-if [ $? == 1 ]; then
-  go get github.com/gsamokovarov/jump
-else
-  echo "jump has already been installed. Skipping"
-fi
-
-# Install Tmux Plugin Manager
-if [ ! -d $HOME/.tmux/plugins/tpm ]; then
-  git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-else
-  echo "tpm has already been installed. Skipping"
-fi
-
-# Install jenv
-if [ ! -d $HOME/.jenv ]; then
-  git clone https://github.com/jenv/jenv.git $HOME/.jenv
-else
-  echo "jenv has already been installed. Skipping"
-fi
 
 # Install tfenv
 if [ ! -d $HOME/.tfenv ]; then 
