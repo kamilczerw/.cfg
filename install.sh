3c52f6d48507cad5f9ee00c8410d558f6525aafd
Kamil Czerwiński <kamil@czerwinski.dev>
2023-02-27 13:45:56 +0000

    Fix krew

--- install.sh
+++ install.sh
@@ -62,8 +62,9 @@ if [ ! -d $HOME/.krew ]; then
     tar zxvf "${KREW}.tar.gz" &&
     "$KREW" install krew
     export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
-    krew install ctx
-    krew install ns
+
+    kubectl krew install ctx
+    kubectl krew install ns
   )
 fi
 
