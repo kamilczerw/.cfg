45940585fbec692bd23e1b09cc4d500c50d4f11f
Kamil Czerwiński <kamil@czerwinski.dev>
2023-08-04 17:43:29 +0200

    Add nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,21 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.to = {
+  install_info = {
+    url = "~/work/playground/tree-sitter-to", -- local path or git repo
+    files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+    -- optional entries:
+    branch = "main", -- default branch in case of git repo if different from master
+    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+  },
+  filetype = "to", -- if filetype does not match the parser name
+}
+
+vim.filetype.add({
+  extension = {
+    to = "to",
+  },
+})
